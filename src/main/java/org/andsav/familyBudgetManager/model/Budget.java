86adaad10ed596341be72171cbaa9c0f979cfa9c
Savand <Savand@Art>
2017-01-08 12:46:02 +0200

    Replaced tab symbol with two spaces

--- src/main/java/org/andsav/familyBudgetManager/model/Budget.java
+++ src/main/java/org/andsav/familyBudgetManager/model/Budget.java
@@ -3,40 +3,40 @@ package org.andsav.familyBudgetManager.model;
 import org.andsav.familyBudgetManager.model.abstractentity.NamedEntity;
 
 public class Budget extends NamedEntity {
-	
-	private int budgetPerDay;
-	
-	private final Integer budgetAmount;
-	
-	private final User budgetCreator;
-	
+  
+  private int budgetPerDay;
+  
+  private final Integer budgetAmount;
+  
+  private final User budgetCreator;
+  
 
-	public Budget(Long id, String budgetName, int budgetPerDay, Integer budgetAmount, User budgetCreator) {
-		super(id, budgetName);
-		this.budgetPerDay = budgetPerDay;
-		this.budgetAmount = budgetAmount;
-		this.budgetCreator = budgetCreator;
-	}
-	
-	public Budget(String budgetName, int budgetPerDay, Integer budgetAmount, User budgetCreator) {
-		this(null, budgetName, budgetPerDay, budgetAmount, budgetCreator);
-	}
-	
-	public int getBudgetPerDay() {
-		return budgetPerDay;
-	}
+  public Budget(Long id, String budgetName, int budgetPerDay, Integer budgetAmount, User budgetCreator) {
+    super(id, budgetName);
+    this.budgetPerDay = budgetPerDay;
+    this.budgetAmount = budgetAmount;
+    this.budgetCreator = budgetCreator;
+  }
+  
+  public Budget(String budgetName, int budgetPerDay, Integer budgetAmount, User budgetCreator) {
+    this(null, budgetName, budgetPerDay, budgetAmount, budgetCreator);
+  }
+  
+  public int getBudgetPerDay() {
+    return budgetPerDay;
+  }
 
-	public void setBudgetPerDay(int budgetPerDay) {
-		this.budgetPerDay = budgetPerDay;
-	}
+  public void setBudgetPerDay(int budgetPerDay) {
+    this.budgetPerDay = budgetPerDay;
+  }
 
-	public Integer getBudgetAmount() {
-		return budgetAmount;
-	}
+  public Integer getBudgetAmount() {
+    return budgetAmount;
+  }
 
-	public User getBudgetCreator() {
-		return budgetCreator;
-	}
+  public User getBudgetCreator() {
+    return budgetCreator;
+  }
 
-	
+  
 }
