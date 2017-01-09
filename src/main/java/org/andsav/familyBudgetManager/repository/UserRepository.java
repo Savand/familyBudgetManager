0254c3ce89d71cdaf6572dda51f0cf485ab0284d
Savand <Savand@Art>
2017-01-09 17:53:51 +0200

    Create application structure

--- src/main/java/org/andsav/familyBudgetManager/repository/UserRepository.java
+++ src/main/java/org/andsav/familyBudgetManager/repository/UserRepository.java
@@ -1,5 +1,17 @@
 package org.andsav.familyBudgetManager.repository;
 
-public interface UserRepository {
+import java.util.List;
 
+import org.andsav.familyBudgetManager.model.User;
+
+public interface UserRepository {
+  User save(User user);
+  
+  boolean delete(int id);
+  
+  User getUser(int id);
+  
+  User getByEmail(String email);
+  
+  List<User> getAllUsers();
 }
---
+++ src/main/java/org/andsav/familyBudgetManager/service/UserService.java
@@ -0,0 +1,21 @@
+package org.andsav.familyBudgetManager.service;
+
+import java.util.List;
+
+import org.andsav.familyBudgetManager.model.User;
+import org.andsav.familyBudgetManager.util.exception.NotFoundException;
+
+public interface UserService {
+  
+  public User save(User user);
+  
+  public void delete(int id) throws NotFoundException;
+  
+  public User get(int id) throws NotFoundException;
+  
+  public User getBuEmail(String email) throws NotFoundException;
+  
+  public List<User> getAll();
+  
+  public void update(User user) throws NotFoundException;
+}
