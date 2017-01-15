08c8ccf1ac836fcd4a3ee5030ad253edcc0aeb2d
Savand <savka.and@gmail.com>
2017-01-15 11:07:46 +0200

    Edit interfaces in repository layer

--- src/main/java/org/andsav/familyBudgetManager/repository/OutcomeRepository.java
+++ src/main/java/org/andsav/familyBudgetManager/repository/OutcomeRepository.java
@@ -1,5 +1,20 @@
 package org.andsav.familyBudgetManager.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.andsav.familyBudgetManager.model.Outcome;
+
 public interface OutcomeRepository {
 
+  public Outcome save(Outcome outcome);
+  
+  public boolean delete(Integer outcomeId);
+  
+  public Outcome get(Integer outcomeId);
+  
+  public List<Outcome> getByBudgetId(Integer budgetId);
+
+  public List<Outcome> getByBudgetIdBetweenDates(Integer budgetId, LocalDateTime startDate, LocalDateTime endDate);
+  
 }
