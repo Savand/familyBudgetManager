8399fc9ba728120d8fad6a341f473372dfa8406e
Andrii_Savka <Andrii_Savka@EPUALVIW0529.kyiv.epam.com>
2017-05-19 18:45:45 +0300

    Add validation provider

--- src/main/java/org/andsav/family_budget_manager/model/User.java
+++ src/main/java/org/andsav/family_budget_manager/model/User.java
@@ -60,8 +60,7 @@ public class User extends NamedEntity {
     private Byte[] userIcon;
 
     @Column(nullable = false, unique = true)
-    @Email
-    @NotEmpty
+    @Email(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
     private String email;
 
     @Column(nullable = false)
